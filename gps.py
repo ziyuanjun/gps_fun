83b22bf677d9dbbbf2cb39b7cfa5ebbd20538a35
ziyuanjun <xzy_study@163.com>
2018-10-27 22:23:31 +0800

    correct the directory name

--- gps.py
+++ gps.py
@@ -4,8 +4,8 @@ from coordTransform_utils import *
 ISOTIMEFORMAT='%Y-%m-%d_%X'
 ser=Ser.Serial('/dev/ttyUSB0',4800,timeout=5)
 date_time=time.strftime( ISOTIMEFORMAT, time.localtime( time.time() ) )
-filename1='gps_data/data_csv_'+date_time+'.csv'
-filename2='gps_data/data_raw_'+date_time+'.txt'
+filename1='gps_Data/data_csv_'+date_time+'.csv'
+filename2='gps_Data/data_raw_'+date_time+'.txt'
 file_data=file(filename1,'w')
 file_data.write('loc\n')
 file_raw=file(filename2,'w')
@@ -20,12 +20,10 @@ while 1:
         latDirec=splitline[3]
         longitude=splitline[4]
         longDirec=splitline[5]
-        #print line
         print latDirec+": "+latitiude+"  "+longDirec+": "+longitude
-        #print
 
 
-        #
+        #parse longitude and latitiude, transform to GaoDe map
         try:
             latdata=float(latitiude)
             longdata=float(longitude)
@@ -34,10 +32,8 @@ while 1:
             longall=long_deg+(longdata-long_deg*100)/60.0
             latall=lat_deg+(latdata-lat_deg*100)/60.0
             lng_lat_GaoDe=wgs84_to_gcj02(longall,latall)#WGS84->GAODE
-            #print lng_lat_GaoDe
             data=str(lng_lat_GaoDe[0])+","+str(lng_lat_GaoDe[1])
             file_data.write("\""+data+"\"\n")
         except:
             continue
-            
 
